fb678b2394da036e6eab51a5077faa7589a9d69f
Bernardo <bernardo.carvalho@tecnico.ulisboa.pt>
2018-04-18 22:34:35 +0100

    new funtion

--- magnetic-flux-fields.py
+++ magnetic-flux-fields.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Computation of analytical values for magnetic caused by current rings in cylindrical coordinates (R,z,phi)
@@ -14,6 +15,20 @@ import scipy.constants as cnst
 #https://uiuc-cse.github.io/2014-01-30-cse/lessons/thw-scipy/tutorial.html
 import scipy.special as spcl
 
+def subs(a, z0, R, z):
+   """
+   
+   """
+   a2=a*a
+   R2=R*R
+   z2=np.square(z-z0)
+   alpha2=a2 + R2 + z2 - 2.0 * a*R
+   beta2 =a2 + R2 + z2 + 2.0 * a*R
+   #k2=m
+   k2 = 1.0 - alpha2/beta2
+   C = cnst.mu_0 /np.pi
+
+
 def Aphi(a, z0, R, z):
    "rho, z0 coordinates of current ring (I=1A)"
 #   function_suite
@@ -25,7 +40,31 @@ def Aphi(a, z0, R, z):
    #k2=m
    k2 = 1.0 - alpha2/beta2
    C = cnst.mu_0 /np.pi
+   
+   Aphi= ((2 - k2) * spcl.ellipk(k2) - 2 * spcl.ellipe(k2)) / k2
+   Aphi=  Aphi * 4 * a  / np.sqrt(beta2)
+   Aphi=  Aphi * C / 4.0 
+   
    #https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.ellipk.html
    
-   return spcl.ellipk(k2)
+   return Aphi
+
+def Bzloop(a, z):
+   """
+   Field on Axis of Current Loop  (I=1A)
+   Checked against
+   Bzloop(1, 0)    = 6.283185307179586e-07 T
+   Bzloop(0.1, .1) = 2.221441469079183e-06
+   http://hyperphysics.phy-astr.gsu.edu/hbase/magnetic/curloo.html
+       
+   """
+   
+   a2=a*a
+   z2=z*z
+   Bz = cnst.mu_0 /2.0 * a2 / (a2 + z2)**(3./2.)
+   return Bz
+   
+if __name__ == "__main__":
+    a=Aphi(0.46, 0.0, 0.2, 0)
+    b=Bzloop(0.1, .1)
 
