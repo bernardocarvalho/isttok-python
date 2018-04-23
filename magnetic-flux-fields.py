1586bc14544f6bad60aaf5bc8a04e9c71c016d9c
Bernardo <bernardo.carvalho@tecnico.ulisboa.pt>
2018-04-23 21:44:33 +0100

    generating ISTTOK fields

--- magnetic-flux-fields.py
+++ magnetic-flux-fields.py
@@ -57,6 +57,12 @@ def Bloop(a, z0, R, z):
     BR=BR*((a2 + R2 + z2) * spcl.ellipe(k2) - alpha2 * spcl.ellipk(k2))
     return BR, Bz
 
+def BpolBrad(BR,BZ, angles):
+    ""
+    Bpol=BZ* np.cos(angles) - BR* np.sin(angles)
+    Brad=BZ* np.sin(angles) + BR* np.cos(angles)
+    return Bpol, Brad
+
 def Bzloop(a, z):
     """
     Field on Axis of Current Loop  (I=1A)
@@ -77,27 +83,56 @@ if __name__ == "__main__":
     #
     rm = 0.085 #Minor radius
     RM = 0.46   # Major radius
-    Rprb = 0.0935  #Raio do centro às mirnov  9.35  
+    Rmirn = 0.0935  #Raio do centro às mirnov  9.35  
     #Vertical Coils: 4 coils, 5 turns, R1,2=58 [cm],R2,3=35 [cm],z=±7 [cm]
-    #Horizontal Coils: 2 coils , 4 turns, R1,2=58 [cm],z=±7[cm]
     #
     n = 12     # number of probes
     angles_pbr = np.array([(23./24 - i/n)*2*np.pi for i in range(n)])
     #angles_pbr = np.array([(23./24 - i/n)*2*np.pi for i in range(n)])/np.pi*180.0
-    Rprb =RM + Rprb * np.cos(angles_pbr)
-    zprb =Rprb * np.sin(angles_pbr)
+    Rprb =RM + Rmirn * np.cos(angles_pbr)
+    zprb =Rmirn * np.sin(angles_pbr)
     
     a=Aphi(0.46, 0.0, 0.2, 0)
     b=Bzloop(0.46, .1)
-    b=Bzloop(0.46, .1)
-#    br,bz=Bloop(0.46, 0.0, 1e-8, .1)
     
-    br,bz=Bloop(RM, 0.0, Rprb, zprb)
-#    br
-#    bz
-#    br*bz
-    bpol=bz* np.cos(angles_pbr) - br* np.sin(angles_pbr)
-    brad=bz* np.sin(angles_pbr) + br* np.cos(angles_pbr)
-    bpol * 1e7
-    brad * 1e7
+#    br,bz=Bloop(0.46, 0.0, 1e-8, .1)
+    #Wire loop in the center of vessel
+    #Horizontal Coils: 2 coils , 4 turns, R1,2=58 [cm],z=±7[cm]
+    # Rhor=0.58
+    # Zhor =0.07
+
+    #Vertical Coils: 4 coils, 5 turns, R1,2=58 [cm],R2,3=35 [cm],z=±7 [cm]
+    Rhor=0.58
+    Zhor =0.07
+    nturns=5
+    br,bz=Bloop(Rhor, Zhor, Rprb, zprb)
+    BR = -nturns*br
+    BZ = -nturns*bz
+    Zhor = -0.07
+    br,bz=Bloop(Rhor, Zhor, Rprb, zprb)
+    BR -= nturns*br
+    BZ -= nturns*bz
+    
+    Rhor=0.35
+    br,bz=Bloop(Rhor, Zhor, Rprb, zprb)
+    BR -= nturns*br
+    BZ -= nturns*bz
+    Zhor =0.07
+    br,bz=Bloop(Rhor, Zhor, Rprb, zprb)
+    BR -= nturns*br
+    BZ -= nturns*bz
+      
+    bpol, brad = BpolBrad(BR,BZ, angles_pbr)
+    
+    #bpol * 1e7
+    #brad * 1e7
+    plt.figure() 
+    
+    #plt.axes([0.025, 0.025, 0.95, 0.95])
+    plt.quiver(Rprb,zprb,BR,BZ)
+    plt.xlim((0.36, 0.56  ))
+    plt.axis('equal')
+    #plt.ylim((-0.2, 0.2 ))
+    plt.show()
+    
     #plt.plot(angles_pbr, bz, angles_pbr, br)
